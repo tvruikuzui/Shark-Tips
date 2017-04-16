af11bf11ff9c0bea2c3a0b41c3b7452c5407ed98
liranelyadumi <showupisrael@gmail.com>
2017-04-16 12:40:11 +0300

    hh

--- Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/Offers.java
+++ Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/Offers.java
@@ -13,6 +13,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,10 +31,14 @@ import java.net.URL;
 public class Offers extends Fragment {
 
     private ImageView imgOfferOne;
-
+    private FrameLayout frame;
+    private WebView webView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_offers, container, false);
+        frame = (FrameLayout) view.findViewById(R.id.webFrame);
+        frame.setVisibility(View.GONE);
+        webView = (WebView) view.findViewById(R.id.webFrame);
         imgOfferOne = (ImageView) view.findViewById(R.id.imgofferOne);
         Picasso.with(getContext()).load("http://pointshop.co.il/sharkTips/one.png").into(imgOfferOne);
         imgOfferOne.setOnClickListener(new View.OnClickListener() {
