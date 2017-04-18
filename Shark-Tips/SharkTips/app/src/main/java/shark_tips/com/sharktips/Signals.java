606e0f49fe1d1d4842a300bf7443550ef27be94c
liranelyadumi <showupisrael@gmail.com>
2017-04-18 14:41:23 +0300

    signal from server1

--- Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/Signals.java
+++ Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/Signals.java
@@ -23,33 +23,14 @@ public class Signals extends Fragment {
     private ExpandListAdapter adapter;
     private List<Signal> signals;
     private Signal signal;
-    private Button btnAdd;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signals, container, false);
         listView = (ListView) view.findViewById(R.id.listSignals);
-        btnAdd = (Button) view.findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Signal signal = new Signal();
-                signal.setStatus("open");
-                signal.setAction("buy");
-                signal.setCurrency("GBPCAD");
-                signal.setTime("12:00");
-                signal.setPrice("300");
-                signal.setSellStop("1.6610");
-                signal.setSl("1.6650");
-                signal.setTp1("1.6590");
-                signal.setTp2("1.7580");
-                signals.add(signal);
-                listView.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
-            }
-        });
         signals = new ArrayList<>();
         adapter = new ExpandListAdapter(getContext(),signals);
+
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
