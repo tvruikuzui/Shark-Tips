614704b7d6aa5b23daa7abacea3fe1462052efe8
liranelyadumi <showupisrael@gmail.com>
2017-04-23 19:10:23 +0300

    ll

--- Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/MainHome.java
+++ Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/MainHome.java
@@ -12,20 +12,20 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import pl.droidsonroids.gif.GifDrawable;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainHome extends Fragment {
 
-    private WebView webView;
+   // private GifDrawable gifDrawable;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main_home, container, false);
-        webView = (WebView)view.findViewById(R.id.webView);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("http://pointshop.co.il/sharkTips/anim.gif");
+
         return view;
     }
 
