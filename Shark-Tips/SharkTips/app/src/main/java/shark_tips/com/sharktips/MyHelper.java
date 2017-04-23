708bf89b052f605e9d623f272ff7dd46b0860ebe
liranelyadumi <showupisrael@gmail.com>
2017-04-23 17:20:59 +0300

    23.04.17 ::)

---
+++ Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/MyHelper.java
@@ -0,0 +1,56 @@
+package shark_tips.com.sharktips;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by liranelyadumi on 4/23/17.
+ */
+
+public class MyHelper {
+
+    // Save user log.
+    static public void saveToSharedPreferences(Context context, boolean log){
+        SharedPreferences preferences = context.getSharedPreferences("data",Context.MODE_PRIVATE);
+        if (preferences != null){
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("log",log);
+            editor.commit();
+        }else {
+            Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // retrieve the user log.
+    static public boolean getDataFromSharedPreferences(Context context){
+        SharedPreferences preferences = context.getSharedPreferences("data",Context.MODE_PRIVATE);
+        boolean getData = preferences.getBoolean("log",false);
+        return getData;
+    }
+
+    // save user email.
+    static public void saveUserEmailToSharedPreferences(Context context,String email){
+        SharedPreferences preferences = context.getSharedPreferences("data",Context.MODE_PRIVATE);
+        if (preferences != null){
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("email",email);
+            editor.commit();
+        }else {
+            Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    // retrieve user email.
+    static public String getUserEmailFromSharedPreferences(Context context){
+        SharedPreferences preferences = context.getSharedPreferences("data",Context.MODE_PRIVATE);
+        if (preferences != null){
+            String getEmail = preferences.getString("email","Office@shark-tips.com");
+            return getEmail;
+        }
+        return String.valueOf(Log.d("TAG","ERROR"));
+    }
+
+}
