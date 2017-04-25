9aea6789014b053478bbaae6e25c8e5fcea149b4
Shalev Ifrah <shalevi69@gmail.com>
2017-04-25 20:38:09 +0300

    xml in signal

--- Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/MainHome.java
+++ Shark-Tips/SharkTips/app/src/main/java/shark_tips/com/sharktips/MainHome.java
@@ -6,13 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
-
-import pl.droidsonroids.gif.GifDrawable;
 
 
 /**
